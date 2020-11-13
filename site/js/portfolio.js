e592a08a3bef1ed2309d01dd9a43a458a2565ea6
Teiko <teikomancini1993@gmail.com>
2020-11-13 11:49:10 +0100

    namechange ok. gives me violation:click handler took too long error

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -6,3 +6,13 @@ toggleAvatar = () => {
     avatarBoolean = !avatarBoolean
 }
 myAvatar.addEventListener("click", () => toggleAvatar())
+
+/*change name script*/
+let nameButton = document.getElementById("selectName")
+let nameDisplay = document.getElementById("name")
+changeName =() => {
+    /* let name = prompt("Please enter your name")
+    nameDisplay.innerHTML=name */
+    nameDisplay.style.color="white"
+}
+nameButton.addEventListener("click", () => changeName())
