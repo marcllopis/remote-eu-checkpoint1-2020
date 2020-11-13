02af3e9164ecd09c7eb0140a37f8fa105fc895d8
dima <galtd@list.ru>
2020-11-13 20:41:47 +0300

    site is done

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -42,3 +42,37 @@ let links = document.querySelectorAll('a')
 for(let i=0;i<links.length; i++){
     links[i].style.color = '#750ff7'
 }
+
+
+let btn2 = document.querySelector('.btn2')
+btn2.addEventListener('click',button2)
+
+
+
+let li = document.querySelectorAll('#front-dev-tools li')
+
+function button2(){
+for(let i=0; i<li.length; i++){
+
+    if( i == 0){
+        li[i].innerHTML = 'VSCode'
+    }
+    else if( i == 1){
+        li[i].innerHTML = 'GitHub'
+    }
+    else{
+        li[i].innerHTML = 'Terminal'
+    }
+   
+}
+}
+
+
+let myul = document.getElementById('myul')
+let btn3 = document.querySelector('.btn3')
+let myinput = document.querySelector('.myinput')
+btn3.addEventListener('click',button3)
+
+function button3(){
+    myul.innerHTML += '<li>' + myinput.value + '</li>'
+}
