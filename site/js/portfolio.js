ea1471b935fdedfab881751869210544ac9d196d
Teiko <teikomancini1993@gmail.com>
2020-11-13 12:04:59 +0100

    bg color change

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -8,11 +8,17 @@ toggleAvatar = () => {
 myAvatar.addEventListener("click", () => toggleAvatar())
 
 /*change name script*/
+/*works but gives me "[Violation] 'click' handler took 2188ms"*/
 let nameButton = document.getElementById("selectName")
 let nameDisplay = document.getElementById("name")
 changeName =() => {
-    /* let name = prompt("Please enter your name")
-    nameDisplay.innerHTML=name */
+    let color = prompt("Please choose a color (HEX)","#750ff7")
+    let name = prompt("Please enter your name")
+    nameDisplay.innerHTML=name
     nameDisplay.style.color="white"
+    console.log(color)
+    document.getElementsByClassName("pink-bg")[0].style.backgroundColor = color
+    /*TODO, MAKE THIS MAP WORK*/
+/*     document.getElementsByClassName("pink-bg").map(e => {e.style.backgroundColor = color}) */
 }
 nameButton.addEventListener("click", () => changeName())
